55d113b6c92e3c2573ec359864b6c9a118e3143d
Daron Peugh <daron@simple.com>
2015-01-10 11:04:26 -0800

    populate session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,24 @@
 class SessionController < ApplicationController
+  
+  skip_before_action :gate_keeper
+  
+  def new
+  end
+  
+  def create
+    guest = Guest.authenticate(params[:email], params[:password])
+    if guest
+      session[:guest_id] = guest.id
+      redirect_to root_path
+    else
+      flash[:notice] = "Invalid email or password"
+      redirect_to root_path
+    end
+  end
+  
+  def destroy
+    session[:guest_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
+  
 end
