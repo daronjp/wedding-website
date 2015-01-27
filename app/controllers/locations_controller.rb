f9a41c1405162e08f2949b50a5e7a05a8b6e4b77
Daron Peugh <daron@simple.com>
2015-01-26 20:26:18 -0800

    track

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,6 +9,11 @@ class LocationsController < ApplicationController
     @coffee = Location.where(:category => 'Coffee').order(:neighborhood)
     @drinks = Location.where(:category => 'Bar').order(:neighborhood)
     @entertainment = Location.where(:category => 'Entertainment').order(:neighborhood)
+    
+    Galileo.create(:controller => 'locations',
+                     :view => 'index',
+                     :user_id => session[:visitor_group],
+                     :session => request.session_options[:id])
   end
   
 end
