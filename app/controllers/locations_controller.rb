d73fb2c641ce7dd811351e86444dd535ea8be2d3
Daron Peugh <daron@simple.com>
2015-01-26 20:34:32 -0800

    consolidate categories

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,14 +1,14 @@
 class LocationsController < ApplicationController
   
   def index
-    @outdoors = Location.where(:category => 'Outdoors').order(:description)
-    @dinner = Location.where(:category => 'Dinner').order(:description)
+    @outdoors = Location.where(:category => 'Outdoors/Entertainment').order(:description)
+    @dinner = Location.where(:category => 'Lunch/Dinner').order(:description)
     @dessert = Location.where(:category => 'Dessert').order(:description)
-    @lunch = Location.where(:category => 'Lunch').order(:description)
+    #@lunch = Location.where(:category => 'Lunch').order(:description)
     @breakfast = Location.where(:category => 'Breakfast').order(:description)
     @coffee = Location.where(:category => 'Coffee').order(:description)
     @drinks = Location.where(:category => 'Bar').order(:description)
-    @entertainment = Location.where(:category => 'Entertainment').order(:description)
+    #@entertainment = Location.where(:category => 'Entertainment').order(:description)
     
     Galileo.create(:controller => 'locations',
                      :view => 'index',
