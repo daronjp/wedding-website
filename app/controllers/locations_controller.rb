094535b040bada1389f9584c66cd14085c72ac08
Daron Peugh <daron@simple.com>
2015-01-26 20:30:47 -0800

    order

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,14 +1,14 @@
 class LocationsController < ApplicationController
   
   def index
-    @outdoors = Location.where(:category => 'Outdoors').order(:neighborhood)
-    @dinner = Location.where(:category => 'Dinner').order(:neighborhood)
-    @dessert = Location.where(:category => 'Dessert').order(:neighborhood)
-    @lunch = Location.where(:category => 'Lunch').order(:neighborhood)
-    @breakfast = Location.where(:category => 'Breakfast').order(:neighborhood)
-    @coffee = Location.where(:category => 'Coffee').order(:neighborhood)
-    @drinks = Location.where(:category => 'Bar').order(:neighborhood)
-    @entertainment = Location.where(:category => 'Entertainment').order(:neighborhood)
+    @outdoors = Location.where(:category => 'Outdoors').order(:description)
+    @dinner = Location.where(:category => 'Dinner').order(:description)
+    @dessert = Location.where(:category => 'Dessert').order(:description)
+    @lunch = Location.where(:category => 'Lunch').order(:description)
+    @breakfast = Location.where(:category => 'Breakfast').order(:description)
+    @coffee = Location.where(:category => 'Coffee').order(:description)
+    @drinks = Location.where(:category => 'Bar').order(:description)
+    @entertainment = Location.where(:category => 'Entertainment').order(:description)
     
     Galileo.create(:controller => 'locations',
                      :view => 'index',
