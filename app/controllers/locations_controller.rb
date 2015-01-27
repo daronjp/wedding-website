48f9d8f7a82e2da0358136d41294a15c4686658b
Daron Peugh <daron@simple.com>
2015-01-26 20:43:17 -0800

    places to visit

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,10 +2,11 @@ class LocationsController < ApplicationController
   
   def index
     @outdoors = Location.where(:category => 'Outdoors/Entertainment').order(:description)
-    @dinner = Location.where(:category => 'Lunch/Dinner').order(:description)
-    @dessert = Location.where(:category => 'Dessert').order(:description)
+    @food = Location.where(:category => 'Food').order(:description)
+    #@dinner = Location.where(:category => 'Lunch/Dinner').order(:description)
+    #@dessert = Location.where(:category => 'Dessert').order(:description)
     #@lunch = Location.where(:category => 'Lunch').order(:description)
-    @breakfast = Location.where(:category => 'Breakfast').order(:description)
+    #@breakfast = Location.where(:category => 'Breakfast').order(:description)
     @coffee = Location.where(:category => 'Coffee').order(:description)
     @drinks = Location.where(:category => 'Bar').order(:description)
     #@entertainment = Location.where(:category => 'Entertainment').order(:description)
