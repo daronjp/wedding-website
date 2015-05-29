b4b445ad75ac0bd6e6b28195c0144437261854a5
Daron Peugh <daron@simple.com>
2015-05-28 19:06:16 -0700

    converting zips to postal to accomodate non-US zips

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,6 +1,6 @@
 class Household < ActiveRecord::Base
   
-  attr_accessible :household_name, :seats, :zip, :id, :email, :logins
+  attr_accessible :household_name, :seats, :zip, :id, :email, :logins, :postal
   has_many :guests
   belongs_to :guest
   
